c24e038bdc30b40abffe5695e96a18b067fbc026
Kaan Çamlıbel <kaancamlibel@hotmail.com>
2025-12-06 15:52:56 +0300

    Bullet updated

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,16 +5,24 @@ public class Bullet : MonoBehaviour
     public float speed = 20f;
     private Rigidbody2D rb;
     public bool goLeft;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         if (goLeft)
+        {
             rb.velocity = Vector2.left * speed;
+            spriteRenderer.flipX = true;
+        }
         else
+        {
             rb.velocity = Vector2.right * speed;
+            spriteRenderer.flipX = false;
+        }
 
-        Destroy(gameObject, 5f); // Destroy bullet after 5 seconds
+        Destroy(gameObject, 5f);
     }
 }
